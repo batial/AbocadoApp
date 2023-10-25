b94490c15299f0509a1fe17a6ca717334a17053d
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-10-25 15:22:41 -0300

    update (components) basic NavBar implemented

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 const About = () => {
   return (
     <div>
-      Esta es la pagina de About
+      <NavBar />
+      <p>Esta es la pagina de About</p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
 
 const home = () => {
   return (
     <div>
+      <NavBar />
       <h1>Hola mundo</h1>
     </div>
   )

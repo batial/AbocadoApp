f482d9691f826a7665d2f94bf0386c65d59f6ac7
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-11-03 15:07:00 -0300

    update (api) get product names by api

--- pages/api/avo/index.js
+++ pages/api/avo/index.js
@@ -3,9 +3,12 @@ import DB from "../../../database/db"
 const allAvos = async (req, res) => {
   const db = new DB()
   const allEntires = await db.getAll()
+  const length = allEntires.length
+
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ data: allEntires }));
+  res.end(JSON.stringify({ data: allEntires , length }));
 };
 
 export default allAvos;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import NavBar from '../components/NavBar/NavBar'
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 const home = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    window.fetch("/api/avo").then((res) =>
+      res.json().then(({ data, length }) => {
+        setProductList(data);
+      })
+    );
+  },[]);
+
   return (
     <div>
       <NavBar />
       <h1>Hola mundo</h1>
+      <div>
+        {productList.map((product) => (
+          <div>{product.name}</div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default home;
